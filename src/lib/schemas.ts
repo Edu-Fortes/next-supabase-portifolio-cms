951955878a950dfa374a22da58c6a92a6b01964d
Edu-Fortes <edu.mfortes@gmail.com>
2025-11-07 12:36:46 -0300

    feat(validation): Add profile schema with validation for full name and display name

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -6,3 +6,12 @@ export const loginSchema = z.object({
     .string()
     .min(6, { message: 'Password must be at least 6 characters long' }),
 });
+
+export const profileSchema = z.object({
+  full_name: z.string().nullable().optional(),
+  display_name: z
+    .string()
+    .min(3, { message: 'Display name must be at least 3 characters long' })
+    .nullable()
+    .optional(),
+});
