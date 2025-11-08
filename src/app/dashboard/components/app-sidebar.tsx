16cb68b5bc9ee7963cccbd184c86b3f558a7344c
Edu-Fortes <edu.mfortes@gmail.com>
2025-11-08 16:25:36 -0300

    feat(sidebar): Pass user and profile data to AppSidebar and NavUser components

--- src/app/dashboard/components/app-sidebar.tsx
+++ src/app/dashboard/components/app-sidebar.tsx
@@ -5,15 +5,23 @@ import {
 } from '@/components/ui/sidebar';
 import NavMain from './nav-main';
 import { NavUser } from './nav-user';
+import { Tables } from '@/types/supabase';
+import { User } from '@supabase/supabase-js';
 
-export function AppSidebar() {
+export function AppSidebar({
+  user,
+  profile,
+}: {
+  user: User;
+  profile: Tables<'profiles'> | null;
+}) {
   return (
     <Sidebar collapsible='icon'>
       <SidebarContent>
         <NavMain />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser />
+        <NavUser user={user} profile={profile} />
       </SidebarFooter>
     </Sidebar>
   );
