3cbdbed1ba9dcf025033baadfcc361e20a15f161
Edu-Fortes <edu.mfortes@gmail.com>
2025-11-12 01:19:22 -0300

    refact(content): update headings and descriptions for clarity in NewContentPage

--- src/app/dashboard/content/new-content/page.tsx
+++ src/app/dashboard/content/new-content/page.tsx
@@ -3,9 +3,9 @@ import { ContentForm } from '../content-form';
 export default function NewContentPage() {
   return (
     <div>
-      <h2 className='font-semibold'>Create New Project</h2>
+      <h2 className='font-semibold'>Create New Content</h2>
       <p className='text-sm text-muted-foreground'>
-        Fill out the details for your portfolio project.
+        Fill out the details for your new portfolio project or blog post.
       </p>
       <ContentForm action='create' />
     </div>
